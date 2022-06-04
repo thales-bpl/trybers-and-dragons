d95e9e7224eb6c938aa62097289b6a666f5b0cd5
thales-bpl <thales.bplima@gmail.com>
2022-06-03 21:49:31 -0300

    impl: setup scripts

---
+++ scripts/nodeterminal.sh
@@ -0,0 +1,1 @@
+docker exec -it trybers_and_dragons bash
---
+++ scripts/start.sh
@@ -0,0 +1,22 @@
+echo $' === === project log === ===
+  === START ===
+  1. Iniciar aplicação:
+    na raíz do projeto: scripts/nodeterminal.sh
+    no terminal do container node: npm start | npm run debug
+
+  === TO DO LIST ===
+
+  === NOTES ===
+
+  === BUG LOG ===
+
+  === SRCs ===
+  Repo: https://github.com/tryber/sd-016-b-project-trybers-and-dragons
+  PR: https://github.com/tryber/sd-016-b-project-trybers-and-dragons
+
+  === README FIX ===
+
+  === === end of log === ===
+'
+
+docker-compose up --force-recreate -d;
